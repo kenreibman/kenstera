f133a940f884bddad5576eaad8d69bfe9e7308e0
kenreibman <kreibman@alumni.albany.edu>
2025-12-29 19:05:38 -0500

    Font size change

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -5,11 +5,11 @@ export function Hero() {
     <section className="bg-white text-black">
       <div className="mx-auto max-w-6xl px-3 sm:px-6 py-12 lg:py-16">
         <div className="mx-auto max-w-5xl text-center">
-          <h1 className="text-balance font-semibold leading-[1.05] tracking-[-0.02em] text-6xl sm:text-7xl text-left sm:text-center">
+          <h1 className="text-balance font-semibold leading-[1.05] tracking-[-0.02em] text-5xl sm:text-7xl text-left sm:text-center">
             The design agency your brand has been looking for
           </h1>
 
-          <p className="mx-auto mt-6 max-w-3xl text-left sm:text-center text-balance sm:text-pretty text-sm leading-6 text-black/70 sm:text-2xl sm:leading-7">
+          <p className="mx-auto mt-6 max-w-3xl text-left sm:text-center text-balance sm:text-pretty text-lg leading-6 text-black/70 sm:text-2xl sm:leading-7">
             Kenstera builds experiences that grow with your ambitions. Driven by thoughtful design, smart technology, and a future-ready mindset.
           </p>
         </div>

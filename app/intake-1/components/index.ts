2d4a5bc348c2b4f1650f0fe312e9961a1d8f4ec7
kenreibman <kreibman@alumni.albany.edu>
2026-01-20 21:10:22 -0500

    build fix

--- app/intake-1/components/index.ts
+++ app/intake-1/components/index.ts
@@ -1,14 +1,14 @@
 // Local Hero with modified headline
 export { default as Hero } from './Hero'
 
-// Re-export all other components from intake-1
-export { StickyNav } from '../../intake-1/components'
-export { CredibilityStrip } from '../../intake-1/components'
-export { ProblemSection } from '../../intake-1/components'
-export { HowItWorks } from '../../intake-1/components'
-export { Deliverables } from '../../intake-1/components'
-export { SecurityCompliance } from '../../intake-1/components'
-export { CaseStudy } from '../../intake-1/components'
-export { Objections } from '../../intake-1/components'
-export { FAQ } from '../../intake-1/components'
-export { FinalCTA } from '../../intake-1/components'
+// Re-export all other components from intake
+export { StickyNav } from '../../intake/components'
+export { CredibilityStrip } from '../../intake/components'
+export { ProblemSection } from '../../intake/components'
+export { HowItWorks } from '../../intake/components'
+export { Deliverables } from '../../intake/components'
+export { SecurityCompliance } from '../../intake/components'
+export { CaseStudy } from '../../intake/components'
+export { Objections } from '../../intake/components'
+export { FAQ } from '../../intake/components'
+export { FinalCTA } from '../../intake/components'
